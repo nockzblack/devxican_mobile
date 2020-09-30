efcdb8105be7c8d9c6005107cb7b901180b3db27
Fer Benavides <nockzblack@gmail.com>
2020-09-29 23:12:42 -0500

    Interface and backend for users sign up

---
+++ app/src/main/java/com/example/devxican_mobile/LogInActivity.java
@@ -0,0 +1,103 @@
+package com.example.devxican_mobile;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LogInActivity extends AppCompatActivity {
+
+
+
+    private static final String TAG = "LogInActivity";
+
+    private FirebaseAuth mAuth; // firebase object
+
+    private Button signUp;
+    private Button logIn;
+
+    private EditText emailInput;
+    private EditText passwordInput;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        emailInput = findViewById(R.id.editTextEmail);
+        passwordInput = findViewById(R.id.editTextPassword);
+
+        signUp = findViewById(R.id.buttonSignUp);
+        logIn = findViewById(R.id.buttonLogIn);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+    /*
+        // [START create_user_with_email]
+
+        // [END create_user_with_email]
+    */
+
+    }
+
+
+    public void makeSignUp(View v) {
+        //Toast.makeText(this, "BUTTON REGISTRARSE PRESSED", Toast.LENGTH_SHORT).show();
+        String email = this.emailInput.getText().toString();
+        String password =  this.passwordInput.getText().toString();
+
+        if (validateEmailPassword(email,password)) {
+            authServiceNewUserEmailPassword(email, password);
+        }
+
+
+    }
+
+    public void makeLogIn(View v) {
+        Toast.makeText(this, "BUTTON INICIAR SESSION PRESSED", Toast.LENGTH_SHORT).show();
+    }
+
+    private void authServiceNewUserEmailPassword(final String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            CharSequence msg = "Usuario con correo: " + user.getEmail() + " creado exitosamente";
+                            Toast.makeText(LogInActivity.this, msg, Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(LogInActivity.this, "Error al crear usuario con correo: ." + email,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
+    private boolean validateEmailPassword(String mail, String password) {
+        return true;
+    }
+
+
+}
