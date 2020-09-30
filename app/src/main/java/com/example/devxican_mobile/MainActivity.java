af987b53c350a748e597f139e1a006990f09a05d
Fer Benavides <nockzblack@gmail.com>
2020-09-29 21:05:53 -0500

    firebase added to project

--- app/src/main/java/com/example/devxican_mobile/MainActivity.java
+++ app/src/main/java/com/example/devxican_mobile/MainActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
