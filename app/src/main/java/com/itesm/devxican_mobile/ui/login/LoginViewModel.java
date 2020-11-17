4945d85c1de69e9adf8362a3b3d370dd792ce501
Fer Benavides <nockzblack@gmail.com>
2020-11-16 18:49:08 -0600

    login complete
    Signed-off-by: Fer Benavides <nockzblack@gmail.com>

--- app/src/main/java/com/itesm/devxican_mobile/ui/login/LoginViewModel.java
+++ app/src/main/java/com/itesm/devxican_mobile/ui/login/LoginViewModel.java
@@ -1,24 +1,40 @@
 package com.itesm.devxican_mobile.ui.login;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import android.util.Log;
 import android.util.Patterns;
 
-import com.itesm.devxican_mobile.data.LoginRepository;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.itesm.devxican_mobile.data.Result;
 import com.itesm.devxican_mobile.data.model.LoggedInUser;
 import com.itesm.devxican_mobile.R;
+
+import java.util.concurrent.Executor;
 
 public class LoginViewModel extends ViewModel {
 
     private MutableLiveData<LoginFormState> loginFormState = new MutableLiveData<>();
     private MutableLiveData<LoginResult> loginResult = new MutableLiveData<>();
-    private LoginRepository loginRepository;
 
-    LoginViewModel(LoginRepository loginRepository) {
-        this.loginRepository = loginRepository;
+
+    private static final String TAG = "LoginViewModel";
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private LoginActivity activity;
+
+
+    LoginViewModel(FirebaseAuth mAuth, LoginActivity activity) {
+        this.mAuth = mAuth;
+        this.activity = activity;
     }
 
     LiveData<LoginFormState> getLoginFormState() {
@@ -29,16 +45,26 @@ public class LoginViewModel extends ViewModel {
         return loginResult;
     }
 
-    public void login(String username, String password) {
-        // can be launched in a separate asynchronous job
-        Result<LoggedInUser> result = loginRepository.login(username, password);
+    public void login(String email, String password) {
 
-        if (result instanceof Result.Success) {
-            LoggedInUser data = ((Result.Success<LoggedInUser>) result).getData();
-            loginResult.setValue(new LoginResult(new LoggedInUserView(data.getDisplayName())));
-        } else {
-            loginResult.setValue(new LoginResult(R.string.login_failed));
-        }
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.wtf(TAG, "createUserWithEmail:success");
+
+                            LoggedInUser user =  new LoggedInUser(mAuth.getCurrentUser());
+
+                            Result<LoggedInUser> result = new Result.Success<>(user);
+
+                            LoggedInUser data = ((Result.Success<LoggedInUser>) result).getData();
+                            loginResult.setValue(new LoginResult(new LoggedInUserView(data.getDisplayName())));
+                        } else {
+                            loginResult.setValue(new LoginResult(R.string.login_failed));
+                        }
+                    }
+                });
     }
 
     public void loginDataChanged(String username, String password) {
@@ -59,7 +85,7 @@ public class LoginViewModel extends ViewModel {
         if (username.contains("@")) {
             return Patterns.EMAIL_ADDRESS.matcher(username).matches();
         } else {
-            return !username.trim().isEmpty();
+            return false;
         }
     }
 
@@ -68,3 +94,84 @@ public class LoginViewModel extends ViewModel {
         return password != null && password.trim().length() > 5;
     }
 }
+
+
+/*
+    @Override
+public void onStart() {
+    super.onStart();
+    // Check if user is signed in (non-null) and update UI accordingly.
+    FirebaseUser currentUser = mAuth.getCurrentUser();
+    updateUI(currentUser);
+}
+
+
+SIGN UP NEW USERS
+
+mAuth.createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI(user);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+
+                // ...
+            }
+        });
+
+
+        SIGN IN EXITING USERS
+
+        mAuth.signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI(user);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                    Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+
+                // ...
+            }
+        });
+
+
+        Access user information
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+if (user != null) {
+    // Name, email address, and profile photo Url
+    String name = user.getDisplayName();
+    String email = user.getEmail();
+    Uri photoUrl = user.getPhotoUrl();
+
+    // Check if user's email is verified
+    boolean emailVerified = user.isEmailVerified();
+
+    // The user's ID, unique to the Firebase project. Do NOT use this value to
+    // authenticate with your backend server, if you have one. Use
+    // FirebaseUser.getIdToken() instead.
+    String uid = user.getUid();
+}
+
+
+
+ */
